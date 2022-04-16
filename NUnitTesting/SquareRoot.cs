aa5e7f1b59260bef12f103132735a686b6985713
Imransk7768 <imransk7768@gmail.com>
2022-04-17 00:57:01 +0530

    [Imran] Added: Checking Square Root Values implemented

---
+++ NUnitTesting/SquareRoot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class SquareRoot
+    {
+        public void Sqaure()
+        {
+            Console.WriteLine("Enter the Positive Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            double root = Math.Sqrt(num);
+            Console.WriteLine("Square root of the " + num + " number is: " + Root);
+        }
+    }
+}
