637eee41562ff97785b57a9d2b85646cea288430
Imransk7768 <imransk7768@gmail.com>
2022-04-16 22:56:55 +0530

    [Imran] Added: Welcome to N-Unit Logic Programs

---
+++ NUnitTesting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnitTesting;
+
+namespace NUnitTesting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(">>>>>>> Welcome to N-Unit Logical Programs <<<<<<<");
+        }
+
+    }
+}
