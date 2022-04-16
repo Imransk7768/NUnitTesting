d0e76577426054a4e35756f28c5b8c302b745b0e
Imransk7768 <imransk7768@gmail.com>
2022-04-16 23:15:24 +0530

    [Imran] Added: Vending Machine Implemented

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -7,7 +7,28 @@ namespace NUnitTesting
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(">>>>>>> Welcome to N-Unit Logical Programs <<<<<<<");
+            Console.WriteLine(">>>>>>> Welcome to Logical Programs <<<<<<<");
+            bool end = true;
+            Console.WriteLine("1. Vending Machine\n2. End Of The Program");
+            while (end == true)
+            {
+                Console.WriteLine("\nEnter an Option to Exicute.");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        VendingMachine machine = new VendingMachine();
+                        machine.Counting();
+                        break;
+                    case 2:
+                        end = false;
+                        Console.WriteLine("Program Is Ended.");
+                        break;
+                    default:
+                        Console.WriteLine("Enter the Valid Option");
+                        break;
+                }
+            }
         }
 
     }
---
+++ NUnitTesting/VendingMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class VendingMachine
+    {
+        public void Counting()
+        {
+            int[] notes = { 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+            int[] frequency = new int[notes.Length];
+            Console.WriteLine("Enter Amount");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            for (int index = 0; index < notes.Length; index++)
+            {
+                if (amount > 0)
+                {
+                    frequency[index] = amount / notes[index];
+                    amount -= frequency[index] * notes[index];
+                }
+            }
+            for (int index = 0; index < frequency.Length; index++)
+            {
+                if (frequency[index] != 0)
+                {
+                    Console.WriteLine(notes[index] + "   " + frequency[index]);
+                }
+            }
+        }
+    }
+}
